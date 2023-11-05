241e25a8fbcb6dfc310ebfb748d431fae73fa176
An Pham <an.phxm@gmail.com>
2023-11-05 15:46:00 -0500

    extracted imdb data

---
+++ imdb.py
@@ -0,0 +1,8 @@
+from sklearn.datasets import fetch_20newsgroups
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+
